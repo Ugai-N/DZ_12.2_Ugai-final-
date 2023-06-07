7cc78fd9e8b6a4d7d055cae7bf12718f41ab8ef9
Nadezhda Ugay <777ugay@gmail.com>
2023-06-07 17:39:03 +0300

    upgrade tests coverage

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -11,3 +11,6 @@ def test_get():
 def test_slice():
     assert arrs.my_slice([1, 2, 3, 4], 1, 3) == [2, 3]
     assert arrs.my_slice([1, 2, 3], 1) == [2, 3]
+    assert arrs.my_slice([], 1, 3) == []
+    assert arrs.my_slice([1, 2, 3, 4, 5, 6], -4, 5) == [3, 4, 5]
+    assert arrs.my_slice([1, 2, 3, 4, 5, 6], -10, 5) == [1, 2, 3, 4, 5]
