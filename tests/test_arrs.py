fd769eb74ae4c555404b7910ff02eda4574b1f29
Nadezhda Ugay <777ugay@gmail.com>
2023-06-07 20:36:10 +0300

    upgrade test_arrs (use fixture)

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -1,16 +1,22 @@
+import pytest
 from utils import arrs
 
 
-def test_get():
-    assert arrs.get([1, 2, 3], 1, "test") == 2
-    assert arrs.get([1, 2, 3], -1, "test") == "test"
-    assert arrs.get([1, 2, 3], 5, "test") == "test"
+@pytest.fixture
+def long_coll():
+    return [1, 2, 3, 4, 5, 6]
+
+
+def test_get(long_coll):
+    assert arrs.get(long_coll, 1, "test") == 2
+    assert arrs.get(long_coll, -1, "test") == "test"
+    assert arrs.get(long_coll, 7, "test") == "test"
     assert arrs.get([], 0, "test") == "test"
 
 
-def test_slice():
-    assert arrs.my_slice([1, 2, 3, 4], 1, 3) == [2, 3]
-    assert arrs.my_slice([1, 2, 3], 1) == [2, 3]
+def test_slice(long_coll):
+    assert arrs.my_slice(long_coll, 1, 3) == [2, 3]
+    assert arrs.my_slice(long_coll, 1) == [2, 3, 4, 5, 6]
     assert arrs.my_slice([], 1, 3) == []
-    assert arrs.my_slice([1, 2, 3, 4, 5, 6], -4, 5) == [3, 4, 5]
-    assert arrs.my_slice([1, 2, 3, 4, 5, 6], -10, 5) == [1, 2, 3, 4, 5]
+    assert arrs.my_slice(long_coll, -4, 5) == [3, 4, 5]
+    assert arrs.my_slice(long_coll, -10, 5) == [1, 2, 3, 4, 5]
